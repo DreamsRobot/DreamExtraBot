4805287be2eccddff14eaf921ac34022edda9501
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-09-15 18:59:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from pyrogram import Client
+from DreamExtraBot.config import Config
+from DreamExtraBot.utils.logger import logger
+import DreamExtraBot.handlers.quote
 
+app = Client(
+    "DreamExtraBot",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+)
+
+if __name__ == "__main__":
+    logger.info("🚀 DreamExtraBot is starting...")
+    app.run()
