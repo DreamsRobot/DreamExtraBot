f8eac2207b47285485a8e9845f105a76239acaee
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-09-15 19:16:00 +0530

    Update config.py

--- DreamExtraBot/config.py
+++ DreamExtraBot/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", "12345"))
-    API_HASH = os.getenv("API_HASH", "your_api_hash")
+    API_ID = int(os.getenv("API_ID", "24995612"))
+    API_HASH = os.getenv("API_HASH", "69c7efab5fa104ed4ad3f9db5cacd96e")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-    OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+    OWNER_ID = int(os.getenv("OWNER_ID", "6934809750"))
